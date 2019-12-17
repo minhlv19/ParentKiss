0c4295b895a7abdfc803988c91b7ad531b7b3ff1
AssReact <huybinh020403@gmail.com>
2019-12-17 17:55:44 +0700

    update

--- src/theme/pages/Home.js
+++ src/theme/pages/Home.js
@@ -1,53 +1,42 @@
 import React, {Component} from 'react';
-import {View, Text, Picker, StyleSheet} from 'react-native';
-
+import {View, Text, Picker, TouchableOpacity} from 'react-native';
+import {Logo, MenuButton} from "../components/header";
 
 class Home extends Component {
+    static navigationOptions = ({navigation}) => {
+        return {
+            headerLeft: <MenuButton onPress={() => navigation.openDrawer()}/>,
+            headerTitle: <Logo/>,
+            headerBackTitle: "Home",
+            headerLayoutPreset: "center"
+        };
+    };
+
     constructor(props) {
         super(props);
-        this.state = {
-        };
-    }
-    render() {
+        this.state = {};
 
+    }
+
+    render() {
         return (
-            <View>
-                <View style={{flex: 1, flexDirection: 'row'}}>
-                    <View style={styles.textpicek}>
+            <View style={{flex: 1, flexDirection: 'row'}}>
+                <View style={{flex: 3}}>
+                    <View style={{flex: 1, flexDirection: 'row',}}>
                         <Picker
                             selectedValue={this.state.language}
-                            style={{}}
-                            onValueChange={(itemValue, itemIndex) =>
-                                this.setState({language: itemValue})
-                            }
-                        >
-                            <Picker.Item label="" value=""/>
-                            <Picker.Item label="Java" value="java1122"/>
-                            <Picker.Item label="Android 1" value="java12"/>
-                        </Picker>
-                    </View>
-                    <View style={styles.textpicek}>
-                        <Picker
-                            style={{flex: 1}}
-                            selectedValue={this.state.language}
-                            onValueChange={(itemValue1, itemIndex) =>
-                                this.setState({language: itemValue1})
+                            style={{height: 50, width: 100}}
+                            onValueChange={(itemValue2, itemIndex) =>
+                                this.setState({language: itemValue2})
                             }>
-                            <Picker.Item label="Java 1" value="java1"/>
-                            <Picker.Item label="JavaScript 2" value="js1"/>
+                            <Picker.Item label="Java2" value="java2"/>
+                            <Picker.Item label="JavaScript1" value="js1"/>
                         </Picker>
                     </View>
                 </View>
-
-                {/*<Text>SDFGHJK</Text>*/}
             </View>
         );
     }
 }
 
 export default Home;
-const styles = StyleSheet.create({
-    textpicek: {
-        flex: 1, margin: 10, backgroundColor: '#CFCFCF', width: 100, height: 40
-    },
-})
